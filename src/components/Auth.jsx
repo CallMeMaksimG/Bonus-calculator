155385bcb965ee0265abc83bb6de44a24d7ec2aa
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-09 13:29:49 +0300

    Добавить валидацию формы регистрации

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,6 +1,8 @@
-import { Link } from "react-router-dom";
+
+import { Link } from 'react-router-dom';
 
 const Auth = () => {
+
     return (
         <div className="auth">
             <h3 className="title-3">Авторизация</h3>
@@ -11,7 +13,9 @@ const Auth = () => {
                 <input type="password" placeholder="Введите пароль" />
                 <button type="submit">Войти</button>
             </form>
-            <Link to="../registration" className="auth__link">Регистрация</Link>
+            <Link to="../registration" className="auth__link">
+                Регистрация
+            </Link>
         </div>
     );
 };
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,19 +1,127 @@
+import { useEffect, useState } from 'react';
+
 function Registraton() {
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [loginDirty, setLoginDirty] = useState(false);
+    const [passwordDirty, setPasswordDirty] = useState(false);
+    const [confirmPasswordDirty, setConfirmPasswordDirty] = useState(false);
+    const [loginError, setLoginError] = useState(
+        'Поле обязательно для заполнения'
+    );
+    const [passwordError, setPasswordError] = useState(
+        'Поле обязательно для заполнения'
+    );
+    const [confirmPasswordError, setConfirmPasswordError] = useState(
+        'Поле обязательно для заполнения'
+    );
+    const [formValid, setFormValid] = useState(false);
+
+    useEffect(() => {
+        if (loginError || passwordError || confirmPasswordError) {
+            setFormValid(false);
+        } else {
+            setFormValid(true);
+        }
+    }, [loginError, passwordError, confirmPasswordError]);
+
+    const loginHandler = (e) => {
+        setLogin(e.target.value);
+        if (e.target.value === ' ') {
+            setLoginError('Некорректный логин');
+        } else {
+            setLoginError('');
+        }
+    };
+
+    const passwordHandler = (e) => {
+        setPassword(e.target.value);
+        if (e.target.value === ' ') {
+            setPasswordError('Некорректный пароль');
+        } else {
+            setPasswordError('');
+        }
+    };
+
+    const confirmPasswordHandler = (e) => {
+        setConfirmPassword(e.target.value);
+        if (e.target.value !== password) {
+            setConfirmPasswordError('Пароли не совпадают');
+        } else {
+            setConfirmPasswordError('');
+        }
+    };
+
+    const blurHandler = (e) => {
+        switch (e.target.name) {
+            case 'login':
+                setLoginDirty(true);
+                break;
+            case 'password':
+                setPasswordDirty(true);
+                break;
+            case 'confirmPassword':
+                setConfirmPasswordDirty(true);
+                break;
+        }
+    };
+
     return (
         <div className="registration">
             <h3 className="title-3">Регистрация</h3>
             <form className="registration__form">
-                <label htmlFor="">Логин</label>
-                <input type="text" placeholder="Придумайте логин" />
-                <label htmlFor="">Пароль</label>
-                <input type="password" placeholder="Придумайте пароль" />
-                <label htmlFor="">Повторите пароль</label>
-                <input type="password" placeholder="Введите пароль еще раз" />
-                <button type="submit">Зарегистрироваться</button>
+                <label htmlFor="login">Логин</label>
+                {loginDirty && loginError && (
+                    <div className="registration__form-error">{loginError}</div>
+                )}
+                <input
+                    onBlur={(e) => blurHandler(e)}
+                    onChange={(e) => loginHandler(e)}
+                    className={loginDirty && loginError ? 'error' : ''}
+                    type="text"
+                    placeholder="Придумайте логин"
+                    name="login"
+                    value={login}
+                />
+                <label htmlFor="password">Пароль</label>
+                {passwordDirty && passwordError && (
+                    <div className="registration__form-error">
+                        {passwordError}
+                    </div>
+                )}
+                <input
+                    onBlur={(e) => blurHandler(e)}
+                    onChange={(e) => passwordHandler(e)}
+                    className={passwordDirty && passwordError ? 'error' : ''}
+                    type="password"
+                    placeholder="Придумайте пароль"
+                    name="password"
+                    value={password}
+                />
+                <label htmlFor="confirmPassword">Повторите пароль</label>
+                {confirmPasswordDirty && confirmPasswordError && (
+                    <div className="registration__form-error">
+                        {confirmPasswordError}
+                    </div>
+                )}
+                <input
+                    onBlur={(e) => blurHandler(e)}
+                    onChange={(e) => confirmPasswordHandler(e)}
+                    className={
+                        confirmPasswordDirty && confirmPasswordError
+                            ? 'error'
+                            : ''
+                    }
+                    type="password"
+                    placeholder="Введите пароль еще раз"
+                    name="confirmPassword"
+                    value={confirmPassword}
+                />
+                <button disabled={!formValid} type="submit">Зарегистрироваться</button>
             </form>
         </div>
     );
 }
-
 
 export default Registraton;
