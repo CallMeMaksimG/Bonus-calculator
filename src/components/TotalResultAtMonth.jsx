1a3fe38a40363dce8c15e081efc462c42bcd27ae
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-10 12:04:38 +0300

    Рефакторинг кода компонента TotalResultAtMonth

--- src/components/TotalResultAtMonth.jsx
+++ src/components/TotalResultAtMonth.jsx
@@ -1,27 +1,21 @@
+const totalCalculator = (arr, key) => {
+    return arr
+        .reduce(
+            (acc, curentValue) => Math.round(acc + Number(curentValue[key])),
+            0
+        )
+        .toLocaleString();
+};
 const TotalResultAtMonth = ({ array }) => {
     return (
         <div className="total-result">
             <div className="total-result__sales">
                 <p>Итоговая сумма продаж за месяц</p>
-                <span>
-                    {array.reduce(
-                        (acc, curentValue) =>
-                            Math.round(acc + Number(curentValue.price)),
-                        0
-                    )}{' '}
-                    &#8381;
-                </span>
+                <span>{totalCalculator(array, 'price')} &#8381;</span>
             </div>
             <div className="total-result__bonus">
                 <p>К выплате %</p>
-                <span>
-                    {array.reduce(
-                        (acc, curentValue) =>
-                            Math.round(acc + Number(curentValue.bonus)),
-                        0
-                    )}{' '}
-                    &#8381;
-                </span>
+                <span>{totalCalculator(array, 'bonus')} &#8381;</span>
             </div>
         </div>
     );
