ee990c9fce7d57abb0710faa7da0fb12020bf90d
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-11 13:24:13 +0300

    Добавить модальное окно для отметки возврата товара

--- src/components/SalesTable/SalesTable.jsx
+++ src/components/SalesTable/SalesTable.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import './SalesTable.scss';
 
 function SalesTable({ percent, array, startDate }) {
+    const [modalOpen, setModalOpen] = useState(false);
+    const onClickSaleItem = (e) => {
+        setModalOpen(true);
+        console.log(e.target.parentNode);
+    }
     const totalCalculator = (arr, key) => {
         return arr
             .reduce(
@@ -13,49 +19,79 @@ function SalesTable({ percent, array, startDate }) {
     const bonusCalculation = (price, percent) =>
         Math.round((Number(price) / 100) * Number(percent));
     return (
-        <table className="sales__table">
-            <caption>{percent} %</caption>
-            <thead>
-                <tr>
-                    <th>Наименование</th>
-                    <th>Цена</th>
-                    <th>К выплате</th>
-                </tr>
-            </thead>
-            {array
-                .filter(
-                    (sale) =>
-                        startDate.getFullYear() === Number(sale.year) &&
-                        startDate.getMonth() === Number(sale.month)
-                )
-                .map((sale) => {
-                    return (
-                        <tbody>
-                            <tr>
-                                <td>{sale.title}</td>
-                                <td>{sale.price} &#8381;</td>
-                                <td>
-                                    {bonusCalculation(sale.price, sale.percent)}{' '}
-                                    &#8381;
-                                </td>
-                            </tr>
-                        </tbody>
-                    );
-                })}
-            <tfoot>
-                <tr>
-                    <td>Итого</td>
-                    <td>
-                        {totalCalculator(array, 'price')}
-                        &#8381;
-                    </td>
-                    <td>
-                        {totalCalculator(array, 'bonus')}
-                        &#8381;
-                    </td>
-                </tr>
-            </tfoot>
-        </table>
+        <>
+            {modalOpen && (
+                <>
+                    <div className="overlay"></div>
+                    <div className="modal-sale">
+                        <div className="modal-sale__item-info">
+                            <p>New Balance 990v3 JJJJound</p>
+                            <p>
+                                <span>Цена</span> 58900 &#8381;
+                            </p>
+                            <p>
+                                <span>К выплате</span> 589 &#8381;
+                            </p>
+                        </div>
+                        <div className="modal-sale__buttons">
+                            <button onClick={() => setModalOpen(false)} className="modal-sale__buttons-close">
+                                Закрыть
+                            </button>
+                            <button className="modal-sale__buttons-return">
+                                Возврат
+                            </button>
+                        </div>
+                    </div>
+                </>
+            )}
+
+            <table className="sales__table">
+                <caption>{percent} %</caption>
+                <thead>
+                    <tr>
+                        <th>Наименование</th>
+                        <th>Цена</th>
+                        <th>К выплате</th>
+                    </tr>
+                </thead>
+                {array
+                    .filter(
+                        (sale) =>
+                            startDate.getFullYear() === Number(sale.year) &&
+                            startDate.getMonth() === Number(sale.month)
+                    )
+                    .map((sale) => {
+                        return (
+                            <tbody>
+                                <tr onClick={onClickSaleItem}>
+                                    <td>{sale.title}</td>
+                                    <td>{sale.price} &#8381;</td>
+                                    <td>
+                                        {bonusCalculation(
+                                            sale.price,
+                                            sale.percent
+                                        )}{' '}
+                                        &#8381;
+                                    </td>
+                                </tr>
+                            </tbody>
+                        );
+                    })}
+                <tfoot>
+                    <tr>
+                        <td>Итого</td>
+                        <td>
+                            {totalCalculator(array, 'price')}
+                            &#8381;
+                        </td>
+                        <td>
+                            {totalCalculator(array, 'bonus')}
+                            &#8381;
+                        </td>
+                    </tr>
+                </tfoot>
+            </table>
+        </>
     );
 }
 
