b9fec4236df1e2ef9bb5f4d931b953903b988d41
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-11-19 19:25:03 +0300

    Исправить баг с отступом кнопки на мобильных телефонах

--- src/components/AddSaleForm/AddSaleForm.jsx
+++ src/components/AddSaleForm/AddSaleForm.jsx
@@ -64,69 +64,73 @@ function AddSaleForm({
         fetchData();
     };
     return (
-        <form onSubmit={onSubmitHandler} className="add-sale__form">
-            <label htmlFor="item">Наименование: </label>
-            <input
-                className="add-sale__form-input"
-                type="text"
-                name="item"
-                placeholder="Введите название товара"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                required
-            />
-            <label htmlFor="price">Цена:</label>
-            <input
-                className="add-sale__form-input"
-                type="number"
-                name="price"
-                placeholder="Введите цену"
-                value={price}
-                onChange={(e) => setPrice(e.target.value)}
-                required
-            />
-            <fieldset>
-                <legend>Процент:</legend>
-                <div className="add-sale__form-radio">
-                    <input
-                        className="add-sale__form-radio-btn"
-                        id="one-percent"
-                        type="radio"
-                        name="radio"
-                        value="1"
-                        checked={percent == '1' ? true : false}
-                        onChange={(e) => setPercent(e.target.value)}
-                        required
-                    />{' '}
-                    <label
-                        htmlFor="one-percent"
-                        className="add-sale__form-radio-label"
-                    >
-                        1%
-                    </label>
+        <div className="add-sale__form-wrapper">
+            <form onSubmit={onSubmitHandler} className="add-sale__form">
+                <label htmlFor="item">Наименование: </label>
+                <input
+                    className="add-sale__form-input"
+                    type="text"
+                    name="item"
+                    placeholder="Введите название товара"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+                <label htmlFor="price">Цена:</label>
+                <input
+                    className="add-sale__form-input"
+                    type="number"
+                    name="price"
+                    placeholder="Введите цену"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    required
+                />
+                <div>
+                    <fieldset className="add-sale__form-radio-wrapper">
+                        <legend>Процент:</legend>
+                        <div className="add-sale__form-radio">
+                            <input
+                                className="add-sale__form-radio-btn"
+                                id="one-percent"
+                                type="radio"
+                                name="radio"
+                                value="1"
+                                checked={percent == '1' ? true : false}
+                                onChange={(e) => setPercent(e.target.value)}
+                                required
+                            />{' '}
+                            <label
+                                htmlFor="one-percent"
+                                className="add-sale__form-radio-label"
+                            >
+                                1%
+                            </label>
+                        </div>
+                        <div className="add-sale__form-radio">
+                            <input
+                                className="add-sale__form-radio-btn"
+                                id="three-percent"
+                                type="radio"
+                                name="radio"
+                                value="3"
+                                checked={percent == '3' ? true : false}
+                                onChange={(e) => setPercent(e.target.value)}
+                            />{' '}
+                            <label
+                                htmlFor="three-percent"
+                                className="add-sale__form-radio-label"
+                            >
+                                3%
+                            </label>
+                        </div>
+                    </fieldset>
                 </div>
-                <div className="add-sale__form-radio">
-                    <input
-                        className="add-sale__form-radio-btn"
-                        id="three-percent"
-                        type="radio"
-                        name="radio"
-                        value="3"
-                        checked={percent == '3' ? true : false}
-                        onChange={(e) => setPercent(e.target.value)}
-                    />{' '}
-                    <label
-                        htmlFor="three-percent"
-                        className="add-sale__form-radio-label"
-                    >
-                        3%
-                    </label>
-                </div>
-            </fieldset>
-            <button className="add-sale__form-btn" type="submit">
-                Добавить
-            </button>
-        </form>
+                <button className="add-sale__form-btn" type="submit">
+                    Добавить
+                </button>
+            </form>
+        </div>
     );
 }
 
