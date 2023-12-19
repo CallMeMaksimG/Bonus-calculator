82cc34a9fa03e7b4e6484e6c5d4b063dae6225ef
Maksim Golovanov <maxgolovanov10@gmail.com>
2023-12-19 15:50:51 +0300

    Добавить отправку доп доходов в БД

--- src/components/AddAdditionalIncomeForm/AddAdditionalIncomeForm.jsx
+++ src/components/AddAdditionalIncomeForm/AddAdditionalIncomeForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import axios from 'axios';
 import Preloader from '../Preloader/Preloader';
 import './AddAdditionalIncomeForm.scss';
 
@@ -15,8 +16,7 @@ function AddAdditionalIncomeForm({
     const [source, setSource] = useState('');
     const [sumIncome, setSumIncome] = useState('');
 
-    const addAdditionalIncomeHandler = (event) => {
-        event.preventDefault();
+    const addAdditionalIncomeHandler = (userId, source, sumIncome) => {
         const newAdditionalIncome = {
             employee_id: userId,
             source: source,
@@ -25,13 +25,43 @@ function AddAdditionalIncomeForm({
             year: date.getFullYear(),
         };
         setAdditionalIncome([...additionalIncome, newAdditionalIncome]);
-        setDisabledFormAdditionalIncome(false);  
+        setDisabledFormAdditionalIncome(false);
         setHideButtons(false);
+    };
+
+    const onSubmitHandler = async (event) => {
+        try {
+            event.preventDefault();
+            // setIsLoading(true);
+            setSource('');
+            setSumIncome('');
+
+            let formData = new FormData();
+            formData.append('employee_id', userId);
+            formData.append('source', source);
+            formData.append('sum', sumIncome);
+            formData.append('month', date.getMonth());
+            formData.append('year', date.getFullYear());
+
+            await axios({
+                method: 'post',
+                baseURL: 'http://f0883110.xsph.ru',
+                url: '/additionalIncomes.php',
+                data: formData,
+                config: {
+                    headers: { 'Content-type': 'multipart/form-data' },
+                },
+            });
+            addAdditionalIncomeHandler(userId, source, sumIncome);
+        } catch (error) {
+            console.error(error);
+        }
+        // setIsLoading(false);
     };
     return (
         <div className="add-sale__form-wrapper form-wrapper">
             {isLoading && <Preloader />}
-            <form onSubmit={addAdditionalIncomeHandler} className="form">
+            <form onSubmit={onSubmitHandler} className="form">
                 <label htmlFor="item">Источник: </label>
                 <input
                     className="form__input"
