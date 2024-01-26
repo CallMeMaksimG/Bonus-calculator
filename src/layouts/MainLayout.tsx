471a77fa66d8f397c8ba0ce5a4761f739173fcd2
Maksim Golovanov <maxgolovanov10@gmail.com>
2024-01-26 17:37:09 +0300

    Типизация MaynLayout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
 import Header from '../components/Header/Header';
 
-const MainLayout = () => {
-    const [isLoaded, setIsLoaded] = useState(false);
+const MainLayout = (): JSX.Element => {
+    const [isLoaded, setIsLoaded] = useState<boolean>(false);
 
     const loadApp = () => {
         setIsLoaded(true);
